e685734e443cd0d054e10f7e29fe7db6ba4fd29d
Perrine2023 <perrine2004@hotmail.com>
2024-09-23 20:02:53 +0200

    trombi_2

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ L.marker([50.725231, 1.613334]).addTo(map)
     L.marker([50.95194, 1.85635]).addTo(map)
     .bindPopup('Calais, Nord-Pas-de-Calais')
     .openPopup();
+
+    L.marker([50.75 , 2.25]).addTo(map)
+    .bindPopup('Saint-Omer, Nord-Pas-de-Calais')
+    .openPopup();
+
+    L.marker([50.731906 , 1.645652]).addTo(map)
+    .bindPopup('Saint-Martin les Boulogne, Nord-Pas-de-Calais')
+    .openPopup();
+
+    L.marker([50.824 , 1.5931]).addTo(map)
+    .bindPopup('Audresselles, Nord-Pas-de-Calais')
+    .openPopup();
+
+    L.marker([ 50.650002 , 1.65]).addTo(map)
+    .bindPopup('Condette, Nord-Pas-de-Calais')
+    .openPopup();
+
+    L.marker([  50.950001 , 1.85635]).addTo(map)
+    .bindPopup('Calais, Nord-Pas-de-Calais')
+    .openPopup();
+
+    L.marker([ 50.700001 , 1.56667]).addTo(map)
+    .bindPopup('Le Portel, Nord-Pas-de-Calais')
+    .openPopup();
+
+    L.marker([ 50.6384 , 1.74628]).addTo(map)
+    .bindPopup('Samer, Nord-Pas-de-Calais')
+    .openPopup();
